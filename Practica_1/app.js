db9762016ce711aba2a106c67f07ad75a28ef5dd
Sergio Hernandez Casas <shcasas27@gmail.com>
2025-03-10 13:10:43 -0700

    commit

--- Practica_1/app.js
+++ Practica_1/app.js
@@ -1,23 +1,58 @@
 const yargs = require('yargs')
 yargs.version('1.1.0')
+
+
+//comando para agregar
 yargs.command({
-command: 'add',
-describe: 'Add a new note',
-builder: {
-title: {
-describe: 'Note title',
-demandOption: true,
-type: 'string'
-},
-body: {
-describe: 'Note body',
-demandOption: true,
-type: 'string'
-}
-},
-handler: function () {
-    console.log('Title: ' + argv.title)
-    console.log('Body: ' + argv.body)
-}
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
 })
+// comando para eliminar
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// comando para leer
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+
 console.log(yargs.argv)
